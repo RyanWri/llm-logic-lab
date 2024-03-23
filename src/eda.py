ad623e9ea2801bfca7a806f8b0544bfe55d6968c
RyanWri <ranman37@gmail.com>
2024-03-23 16:17:16 +0200

    feature engineering

--- src/eda.py
+++ src/eda.py
@@ -4,6 +4,11 @@ from typing import List
 import pandas as pd
 from plot_utils import *
 from engineer_utils import *
+import numpy as np
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def print_dataframe_stats(df: pd.DataFrame, dataset_type: str) -> None:
@@ -18,13 +23,17 @@ def print_dataframe_stats(df: pd.DataFrame, dataset_type: str) -> None:
 def correlation_to_target(
     df: pd.DataFrame, src_col: str, target_col: str
 ) -> pd.DataFrame:
-    return train_df[[src_col, target_col]].groupby(
-        [src_col], as_index=False
-    ).mean().sort_values(by=target_col, ascending=False)
+    return (
+        train_df[[src_col, target_col]]
+        .groupby([src_col], as_index=False)
+        .mean()
+        .sort_values(by=target_col, ascending=False)
+    )
 
 
 def read_data(filepath: str, dataset_type: str) -> pd.DataFrame:
     return pd.read_csv(os.path.join(filepath, "data", f"{dataset_type}.csv"))
+
 
 def plot_eda(df: pd.DataFrame, target_col: str) -> None:
     plot_histogram(df, src_col="Age", target_col=target_col, bins=20)
@@ -37,22 +46,71 @@ if __name__ == "__main__":
     # read data
     filepath = Path(os.path.abspath(__file__)).parent.parent
     train_df = read_data(filepath, dataset_type="train")
-    
+
     # show features and correlation to survived target label
     target_col = "Survived"
     # print_dataframe_stats(train_df, dataset_type="train")
     # for col in ["Pclass", "Sex", "SibSp", "Parch"]:
     #     print(correlation_to_target(train_df, src_col=col, target_col=target_col))
-    
 
     # exploratory data analysis plots
     # plot_eda(df=train_df, target_col="Survived")
 
     # removing unused features
     print("Before", train_df.shape)
-    train_df = train_df.drop(['Ticket', 'Cabin'], axis=1)
+    train_df = train_df.drop(["Ticket", "Cabin"], axis=1)
     print("After", train_df.shape)
 
     # feature engineering
     train_df["Title"] = extract_title(train_df, col="Name")
+    train_df["Title"] = classify_title(train_df, "Title")
+    # show each title to survive rate
+    print(train_df[["Title", target_col]].groupby(["Title"], as_index=False).mean())
+
+    # map categories to numbers
+    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+    train_df["Title"] = categorial_to_ordinal(train_df, "Title", title_mapping)
+    print(train_df.head())
+
+    # now we can safely remove name and passengerID
+    train_df = train_df.drop(["Name", "PassengerId"], axis=1)
+    print("After removing name and passengerId", train_df.shape)
+
+    # map gender to int
+    gender_mapping = {"female": 1, "male": 0}
+    train_df["Sex"] = categorial_to_ordinal(train_df, "Sex", gender_mapping).astype(int)
+    print(train_df.head())
+
+    # plot age distribution after feature engineering - Missing for now
+
+    logger.info("guess age from distribution")
+    train_df["Age"] = guess_age_from_distribution(train_df).astype(int)
+    print(train_df.head())
+
+    logger.info("split age to ranges")
+    train_df["AgeBand"] = split_age_to_ranges(train_df, bins=5)
+    print(train_df.head())
+
+    logger.info("removing age band after finding ranges")
+    train_df = train_df.drop(["AgeBand"], axis=1)
+
+    logger.info("family size feature")
+    train_df["FamilySize"] = calc_family_size(train_df)
+    print(train_df.head())
+
+
+    logger.info("remove family features")
+    train_df = train_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
+    print(train_df.head())
+
+    logger.info("age times class feature")
+    train_df['Age*Class'] = train_df.Age * train_df.Pclass
+
+    logger.info("ports feature engineering")
+    train_df["Embarked"] = port_to_number(train_df, "Embarked").astype(int)
+    print(train_df.head())
+
+    logger.info("filling missing fare with median data")
+    train_df["Fare"] = train_df['Fare'].fillna(train_df['Fare'].dropna().median())
+    train_df["FareBand"] = split_fare_price_to_ranges(train_df, bins=4).astype(int)
     print(train_df.head())
--- src/plot_utils.py
+++ src/plot_utils.py
@@ -14,25 +14,29 @@ def save_plot(plt: plt, filepath: str, plot_name: str) -> None:
     image = os.path.join(filepath, plot_name)
     plt.savefig(image)
 
+
 def plot_histogram(df: pd.DataFrame, src_col: str, target_col: str, bins: int) -> None:
     g = sns.FacetGrid(df, col=target_col)
     g.map(plt.hist, src_col, bins=bins)
     save_plot(plt, directory, f"histogram_{src_col}_{target_col}.png")
 
+
 def plot_histogram_with_legend(df: pd.DataFrame, src_col: str, target_col: str) -> None:
     grid = sns.FacetGrid(df, col=target_col, row=src_col)
-    grid.map(plt.hist, "Age", alpha= 0.5, bins=20)
+    grid.map(plt.hist, "Age", alpha=0.5, bins=20)
     grid.add_legend()
     save_plot(plt, directory, f"histogram_{src_col}_{target_col}_legend.png")
 
+
 def plot_pointplot(df: pd.DataFrame, src_col: str, target_col: str) -> None:
     grid = sns.FacetGrid(df, row=src_col)
-    grid.map(sns.pointplot, 'Pclass', target_col, 'Sex', palette='deep')
+    grid.map(sns.pointplot, "Pclass", target_col, "Sex", palette="deep")
     grid.add_legend()
     save_plot(plt, directory, f"pointplot_{src_col}_{target_col}.png")
 
+
 def plot_barplot(df: pd.DataFrame, src_col: str, target_col: str) -> None:
     grid = sns.FacetGrid(df, row=src_col, col=target_col)
-    grid.map(sns.barplot, 'Sex', 'Fare', alpha=.5, errorbar=None)
-    grid.add_legend()    
+    grid.map(sns.barplot, "Sex", "Fare", alpha=0.5, errorbar=None)
+    grid.add_legend()
     save_plot(plt, directory, f"barplot_{src_col}_{target_col}.png")
