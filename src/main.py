d6910e04fc61ae73ccfde97bf5c94069e721c47e
Ran Wright <ranman37@gmail.com>
2024-11-22 18:45:58 +0200

    Issue13 (#15)
    * issue13
    
    * aggregate results to 1 file

--- src/main.py
+++ src/main.py
@@ -6,6 +6,10 @@ if __name__ == "__main__":
     # Set root dir for all tasks
     root_dir = str(Path(__file__).parent.parent.resolve())
 
-    # task 1 - task 4
+    # task 1 - task 3
     models = ["mistral", "qwen2"]
-    task1_to_task_4(root_dir, models)
+    # make sure not to modify the model results as we depend on them in the word document
+    # task1_to_task_4(root_dir, models)
+
+    # task 4 - Please see section 4 in the word document
+    # task 5 - Please see section 5 in the word document
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 # all files are absolute from root (afeka-nlp-course)
+import os
 
 
 def write_to_file(sentence, reasoning, filename):
@@ -14,3 +15,31 @@ def load_sentences(filename):
     with open(filename, "r") as file:
         sentences = file.readlines()
     return [sentence.strip() for sentence in sentences]
+
+
+def concat_text_files(input_folder: str, output_filename: str) -> None:
+    """Concatenate all text files in a folder and write to output_filename."""
+    try:
+        with open(output_filename, "w") as outfile:
+            for filename in sorted(os.listdir(input_folder)):
+                file_path = os.path.join(input_folder, filename)
+                if os.path.isfile(file_path) and filename.endswith(".txt"):
+                    with open(file_path, "r") as infile:
+                        outfile.write(infile.read())
+                        outfile.write(
+                            "\n"
+                        )  # Add a newline between files for separation
+        print(f"All text files have been concatenated into: {output_filename}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    input_folder = "/Users/ranwright/afeka-courses/afeka-nlp-course/output"
+    output_folder = f"{input_folder}/concat"
+    os.makedirs(output_folder, exist_ok=True)
+    for model in ["mistral", "qwen2"]:
+        concat_text_files(
+            input_folder=f"{input_folder}/{model}",
+            output_filename=f"{output_folder}/{model}.txt",
+        )
