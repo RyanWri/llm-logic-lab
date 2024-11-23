f4c9218e8be4a8ac81d1984d4823dbd8c34f9b60
Ben Remez <47637471+Ben-Remez@users.noreply.github.com>
2024-11-23 16:44:40 +0200

    Complete task 7 - generate new nonsense sentences file and perform tasks 1-4 on them.

--- src/models_handler/ollama_handler.py
+++ src/models_handler/ollama_handler.py
@@ -1,7 +1,7 @@
 import os
 
 import ollama
-from src.utils import load_sentences, write_to_file
+from src.utils import load_sentences, write_to_file, create_nonsense_sentences
 
 
 def generate_reasoning(sentence, model: str):
@@ -13,14 +13,23 @@ def generate_reasoning(sentence, model: str):
     return response.get("response", "")
 
 
-def task1_to_task_4(root_dir: str, models: list[str]):
+def task1_to_task_4(root_dir: str, models: list[str], output_dir_name='output', input_file_name='sentences.txt'):
     """Load sentences and generate reasoning for each, then write to file."""
-    input_filename = f"{root_dir}/data/sentences.txt"
+    input_filename = f"{root_dir}/data/{input_file_name}"
     sentences = load_sentences(input_filename)
 
     for model in models:
-        output_dir = f"{root_dir}/output/{model}"
+        output_dir = f"{root_dir}/{output_dir_name}/{model}"
         os.makedirs(output_dir, exist_ok=True)
         for i, sentence in enumerate(sentences):
             reasoning = generate_reasoning(sentence, model=model)
             write_to_file(sentence, reasoning, f"{output_dir}/reasoning{i}.txt")
+
+
+def task7(root_dir: str, models: list[str]):
+    """Create nonsense sentences file and generate reasoning for each, then write to file"""
+    """Uncomment the 3 lines below to generate new nonsense sentences file"""
+    # input_filename = f"{root_dir}/data/sentences.txt"
+    # output_filename = f"{root_dir}/data/nonsense_sentences.txt"
+    # create_nonsense_sentences(input_filename, output_filename)
+    task1_to_task_4(root_dir, models, 'output_nonsense', 'nonsense_sentences.txt')
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 # all files are absolute from root (afeka-nlp-course)
 import os
+import random
+import string
 
 
 def write_to_file(sentence, reasoning, filename):
@@ -43,3 +45,45 @@ if __name__ == "__main__":
             input_folder=f"{input_folder}/{model}",
             output_filename=f"{output_folder}/{model}.txt",
         )
+
+
+def write_sentences_to_file(filename: str, sentences: list[str]):
+    """Write the sentences to an output file"""
+    try:
+        with open(filename, "w") as nonsense_file:
+            for sentence in sentences:
+                nonsense_file.write(sentence + '\n')
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def replace_random_words(sentence, max_words_to_replace=5):
+    """Replace a random word/number of words in a sentence with random characters"""
+    words = sentence.split()
+    num_words_to_replace = random.randint(1, min(max_words_to_replace, len(words)))
+    word_indices = random.sample(range(len(words)), num_words_to_replace)
+
+    for index in word_indices:
+        random_characters = ''.join(random.choices(string.ascii_lowercase, k=random.randint(4, 6)))
+        if words[index].endswith(',') or words[index].endswith('.'):
+            words[index] = random_characters + words[index][-1]
+        else:
+            words[index] = random_characters
+    return ' '.join(words)
+
+
+def create_nonsense_sentences(input_filename, output_filename, max_words_replacement=5, number_of_sentences=5):
+    """Generate the nonsense sentences file"""
+    sentences = load_sentences(input_filename)
+    if len(sentences) == 0:
+        print(f"No sentences were loaded. Please check input file contents/path! input file name: {input_filename}")
+        return 0
+    chosen_sentence = random.choice(sentences)
+    print(f'Chosen sentence:\n{chosen_sentence}')
+    nonsense_sentences = []
+    for i in range(number_of_sentences):
+        nonsense_sentences.append(replace_random_words(chosen_sentence, max_words_replacement))
+    write_sentences_to_file(output_filename, nonsense_sentences)
+    print(f'Nonsense sentences generated successfully into: {output_filename}')
+
+
