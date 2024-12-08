c34854aa778a00c6653ad891edd81c24a5cba16c
linuxu <linuxu@Ubuntu-GPU-61.academic.management.afeka.local>
2024-12-08 17:14:57 +0200

    gpu fine tune

--- src/task10/fine_tune.py
+++ src/task10/fine_tune.py
@@ -1,5 +1,17 @@
+import torch
 from transformers import TrainingArguments, AutoModelForCausalLM, AutoTokenizer, Trainer
+import os
+
+c = os.getcwd()
+import sys
+
+print(sys.path)
 from src.task10.atomic import load_atomic_dataset
+
+# Check for GPU availability
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
 
 model_name = "distilbert/distilgpt2"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -22,26 +34,28 @@ def tokenize_function(example):
     return tokenized_inputs
 
 
-dataset = load_atomic_dataset(split="validation")
+dataset = load_atomic_dataset(split="test")
+print(dataset[0])  # Inspect one sample
 tokenized_dataset = dataset.map(tokenize_function, batched=True)
 print(tokenized_dataset[0])
 
-model_output_dir = "/Users/ranwright/fine-tuned-models/atomic-distilgpt2"
+model_output_dir = "/home/linuxu/models-logs/distilgpt2-fine-tuned"
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
-
+fp_16_enabled = True if device.type == "cuda" else False
 training_args = TrainingArguments(
     output_dir=model_output_dir,
-    num_train_epochs=2,
-    per_device_train_batch_size=4,
-    gradient_accumulation_steps=2,
+    num_train_epochs=5,
+    per_device_train_batch_size=16,
+    gradient_accumulation_steps=4,
     eval_strategy="no",
     save_strategy="epoch",
-    logging_steps=500,
-    learning_rate=5e-5,
-    fp16=False,
+    logging_steps=1000,
+    learning_rate=2e-5,
+    fp16=fp_16_enabled,
     save_total_limit=2,
     load_best_model_at_end=False,
+    report_to="none",
 )
 
 
@@ -49,7 +63,6 @@ trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=tokenized_dataset,  # Tokenized dataset
-    eval_dataset=tokenized_dataset.select(range(200)),  # Use a subset for validation
     tokenizer=tokenizer,
 )
 
